9916a6beda5b7d7554cfcd4da9ef4301ae3abbe9
George Lowe <georgeloweone@gmail.com>
2021-04-14 16:24:16 +0100

    Updated Information about me

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ projects.set("1", {
   title: "Instagram Automation Bot 💬",
   brief_description:
     "I designed and implemented this bot out of the desire to automate tasks I faced when growing a brand Instagram. Features include, calculating users that do not follow an account back and unfollowing them, liking and commenting on posts by a set of specific users or hashtags and analysing growth data over time.",
-  full_description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis mollitia possimus harum minima? Nemo reprehenderit accusantium molestias, veritatis excepturi dicta sapiente vitae laboriosam quisquam modi animi, ullam omnis, vero ex magnam! Asperiores accusantium dolor, repudiandae reprehenderit autem deserunt eveniet vero eius explicabo quas adipisci expedita in. Sequi velit ullam porro laborum fuga nam vitae quod incidunt in atque! Ratione dolore nesciunt quia exercitationem earum culpa voluptates impedit obcaecati eos ipsa excepturi, repellat, tenetur dolor velit unde, error neque repudiandae. Labore, possimus. Esse nisi quo sed dolor asperiores eius, quae veritatis totam praesentium recusandae vero reprehenderit, debitis commodi id numquam!",
+  full_description: "",
   media_src: "",
   code_src: "https://github.com/georgelowe/Instagram-Automation",
   tags: ["Python3", "Selenium"],
@@ -47,7 +46,7 @@ projects.set("2", {
   title: "Camera-based object tracking for VR experiences 📸",
   brief_description:
     "This project introduces a complete system, such that a physical object is tracked using a webcam and reproduced in the same precise location within a virtual environment, allowing users to manipulate the object whilst in virtual reality.",
-  full_description: "Full project description",
+  full_description: "",
   media_src: "Media/Ball.mp4",
   code_src: "https://github.com/georgelowe/VR-Object-Tracking",
   tags: ["Python", "CSharp", "Unity3D", "UDP"],
@@ -57,7 +56,7 @@ projects.set("3", {
   title: "Responsive Portolio Website",
   brief_description:
     "This website that you are currently visiting was designed and produced by me to showcase my work, provide links to my code and to allow potential employers to reach out and contact me.",
-  full_description: "Full project description",
+  full_description: "",
   media_src: "",
   code_src: "https://github.com/georgelowe/georgelowe.github.io",
   tags: ["html", "CSS", "JavaScript"],
@@ -155,7 +154,7 @@ arr = [
   "~ $ george.education()",
   "~ $ george.location()",
   "~ $ george.email()",
-  "~ $ george.languages()",
+  "~ $ george.familiar_languages()",
 ];
 responses_arr = [
   "BSc Computer Science, University of Nottingham",
